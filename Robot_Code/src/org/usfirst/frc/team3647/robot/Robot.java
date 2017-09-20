2e69e186482a9d771b1a7dadac35d9cbfdb11e6f
Surya <suryamadhan@gmail.com>
2017-09-20 10:07:34 -0700

    adding explanations to robot.java

--- Robot_Code/src/org/usfirst/frc/team3647/robot/Robot.java
+++ Robot_Code/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -17,12 +17,14 @@ import team3647subsystems.Joystick007;
 
 public class Robot extends IterativeRobot {
 	
+	//Variables used for conditions in Autonomous
 	double autoSelected;
 	double leftEncoderValues;
 	double rightEncoderValues;
 	boolean leftSide, rightSide;
 	int currentState;
 	
+	//Variables used for moving robot autonomously
 	double straightdist = 1900;
 	double straightDistBR = 1940;
 	double straightDistRR = 1840;
@@ -32,9 +34,12 @@ public class Robot extends IterativeRobot {
 	double backUpDisatnceFromMiddle = 2300;
 	double goForwToCross = 5000;
 
+	//Variables used for conditions in Autonomous
 	boolean steeringWheelDrive = false;
 	int prevState = 0;
+	String switchWiggle = "left";
 	
+	//Creating objects...
 	Encoders enc;
 	Clamp clampObj;
 	DropDown dropdown;
@@ -43,9 +48,9 @@ public class Robot extends IterativeRobot {
 	Joystick007 joyStickValues;
 	CameraServer server;
 	
-	String switchWiggle = "left";
-	
-	
+	//This is the code for the robot initialization
+	//This function runs once before the whole robot starts running
+	//Assign classes for all the objects
 	public void robotInit() 
 	{
 		enc = new Encoders();
@@ -55,11 +60,14 @@ public class Robot extends IterativeRobot {
 		mainpiston = new MainPiston();
 		climberObj = new Climber();
 
+		//This piece of code runs the camera
 		server = CameraServer.getInstance();
 		server.startAutomaticCapture("cam0", 0);
 	}
 
-	
+	//This is the code for the robot initialization
+	//This function runs once before we start the autonomous period\
+	//We reset encoders, make sure the robot is not moving, and all the variables we are going to use are set to the correct value
 	public void autonomousInit() 
 	{
 		currentState = 1;
@@ -70,7 +78,8 @@ public class Robot extends IterativeRobot {
 		rightSide = false; 
 	}
 
-	
+	// This is the piece of code that runs during the Autonomous Period
+	//We use a Digital Pin system to determine what type of autonomous we would like to run so that we would not have to upload code every time
 	public void autonomousPeriodic() 
 	{
 		while(DriverStation.getInstance().isAutonomous() && !DriverStation.getInstance().isDisabled())
@@ -95,167 +104,122 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-	
+	// This piece of code is an emergency auto in case the robot can't read any digital inputs
+	//Case 1: Make sure the Main piston is in position to score a gear
+	//Case 2: Drive straight fast for 2300 ticks
+	//Case 3: Stop the robot and unclamp the piston
 	public void moveForw()
 	{
 		switch(currentState)
 		{
 			case 1:
 				mainpiston.dropMainPiston();
-				Timer.delay(.5);
+				Timer.delay(1);
 				mainpiston.grabMainPiston();
+				Timer.delay(1);
 				currentState = 2;
 				break;
 			case 2:
-				if(enc.returnleftEncValue() < 1900)
+				if(enc.runOrNotForStraight(2000))
 				{
-					Motors007.leftTalon.set(.3);
+					enc.slowDrive(2000);
+
 				}
 				else
 				{
-					leftSide = true;
+					currentState = 3;
+				}
+				break;		
+			case 3:
+				clampObj.unClampThePiston();
+				Motors007.leftTalon.set(0);
+				Motors007.rightTalon.set(0);
+				break;
+		}
+	}
+		
+	//This is the piece of code that will run if we start the robot from the middle position for either color
+	//Case 1: Make sure the Main piston is in position to score a gear
+	//Case 2: Drive straight for 1600 ticks
+	//Case 3: Wiggle and deliver the gear
+	//Case 4: Drive backward for 2 seconds
+	//Case 5: Stop the robot
+	//Case 6: End Case
+	public void centerAuto()
+	{
+		switch(currentState)
+		{
+			case 1:
+				mainpiston.dropMainPiston();
+				Timer.delay(1);
+				mainpiston.grabMainPiston();
+				Timer.delay(1);
+				currentState = 2;
+				break;
+			case 2:
+				if(enc.runOrNotForStraight(distanceforGearFromMiddle))
+				{
+					enc.encoderDrive(distanceforGearFromMiddle, 1);
+				}
+				else
+				{
 					Motors007.leftTalon.set(0);
-				}
-				if(enc.returnrightEncValue() < 1900)
-				{
-					Motors007.rightTalon.set(-.3);
-				}
-				else
-				{
 					Motors007.rightTalon.set(0);
-					rightSide = true;
-				}
-				if(leftSide && rightSide)
-				{
 					currentState = 3;
 				}
 				break;
 			case 3:
+				autoWiggle();
+				mainpiston.dropMainPiston();
 				clampObj.unClampThePiston();
+				Timer.delay(.6);
+				enc.resetEncoders();
+				currentState = 4;
+				break;
+			case 4: 
+				Motors007.leftTalon.set(-.3);
+				Motors007.rightTalon.set(.3);
+				Timer.delay(2);
+				currentState = 5;
+				break;
+			case 5:
+				Motors007.leftTalon.set(0);
+				Motors007.rightTalon.set(0);
+				Timer.delay(.2);
+				mainpiston.grabMainPiston();
+				Timer.delay(.3);
+				enc.resetEncoders();
+				currentState = 6;
+				break;
+			case 6:
+				Motors007.leftTalon.set(0);
+				Motors007.rightTalon.set(0);
 				break;
 		}
 	}
-		
 	
-	public void updateAllPistons()
-	{
-		clampObj.updatePistons();
-		mainpiston.updatePistons();
-		dropdown.updatePistons();
-	}
-	
-	
-	public void updateAllPistonsForSteeringWheel()
-	{
-		clampObj.updatePistons();
-		mainpiston.updatePistons();
-		dropdown.steeringWheelDropDown();
-	}
-	
-
-	public void teleopPeriodic() 
-	{
-		runTeleOp();
-	}
-	
-	
-	public void runTeleOp()
-	{
-		if(steeringWheelDrive)
-		{
-			joyStickValues.updateContollerValues();
-			updateAllPistonsForSteeringWheel();
-			climberObj.theClimber();
-			driveTrainForSteeringWheel();
-		}
-		else
-		{
-			joyStickValues.updateContollerValues();
-			updateAllPistons();
-			climberObj.theClimber();
-			driveTrain();
-		}
-	}
-	
-	
-	public void driveTrain()
-	{
-		if(Joystick007.leftBumper)
-		{
-			teleOpWiggle();
-		}
-		else if(Joystick007.leftTrigger > 0)
-		{
-		
-			sloMoDrive();
-		}
-		else
-		{
-			updatedArcadeDrive();
-		}
-	}
-	
-	
-	public void driveTrainForSteeringWheel()
-	{
-		updatedArcadeDriveForSteeringWheelDrive();
-	}
-	
-	
-	public void testPeriodic() 
-	{
-		// Does N O T H I N G
-	}
-	
-	
-	public void sloMoDrive()
-	{
-		double leftValue, rightValue;
-		double checks,checkt,speed,turn;
-		leftValue = sinx(Joystick007.leftJoySticky);
-		rightValue = Joystick007.rightJoyStickx;
-		
-		checks = Math.abs(leftValue);
-		checkt = Math.abs(rightValue);
-		
-		if (checks <.15) 	
-		{
-			speed = 0;
-		}
-		else 			
-		{
-			speed = .3*(checks * leftValue);
-		}
-		if (checkt <.15) 	
-		{
-			turn = 0;
-		}
-		else 			
-		{
-			if(leftValue == 0)
-			{
-				turn = .3*(rightValue);
-			}
-			else
-			{
-				turn = .3*(rightValue);
-			}
-			
-		}    
-		Motors007.leftTalon.set(((speed+turn)));
-	    Motors007.rightTalon.set(((-speed+turn)));
-	}
-	
-	
+	//This is the piece of code that will run if we start the robot from blue retrieval
+	//Case 1: Make sure the Main piston is in position to score a gear
+	//Case 2: Drive straight fast for 1580 ticks
+	//Case 3: Drive straight slow for 360 ticks
+	//Case 4: ResetEncoders
+	//Case 5: Turn left
+	//Case 6: Set sides to false
+	//Case 7: Wiggle
+	//Case 8: Deliver the gear
+	//Case 9: Turn backward
+	//Case 10: Stop the Robot
+	//Case 11: Cross MidField
+	//Case 12: End Case
 	public void newBlueRetrieval()
 	{
 		switch(currentState)
 		{
 			case 1:
 				mainpiston.dropMainPiston();
-				Timer.delay(1.5);
+				Timer.delay(1);
 				mainpiston.grabMainPiston();
-				Timer.delay(1.5);
+				Timer.delay(1);
 				currentState = 2;
 				break;
 			case 2:
@@ -389,16 +353,28 @@ public class Robot extends IterativeRobot {
 			}
 	}
 	
-	
+	//This is the piece of code that will run if we start the robot from blue retrieval
+	//Case 1: Make sure the Main piston is in position to score a gear
+	//Case 2: Drive straight fast for 1480 ticks
+	//Case 3: Drive straight slow for 360 ticks
+	//Case 4: ResetEncoders
+	//Case 5: Turn right
+	//Case 6: Set sides to false
+	//Case 7: Wiggle
+	//Case 8: Deliver the gear
+	//Case 9: Turn backward
+	//Case 10: Stop the Robot
+	//Case 11: Cross MidField
+	//Case 12: End Case
 	public void newRedRetrieval()
 	{
 		switch(currentState)
 		{
 			case 1:
 				mainpiston.dropMainPiston();
-				Timer.delay(1.5);
+				Timer.delay(1);
 				mainpiston.grabMainPiston();
-				Timer.delay(1.5);
+				Timer.delay(1);
 				currentState = 2;
 				break;
 			case 2:
@@ -544,55 +520,125 @@ public class Robot extends IterativeRobot {
 			}
 	}
 	
-	
-	public void centerAuto()
+	//This is the function that runs during Tele-Operated Period
+	public void teleopPeriodic() 
 	{
-		switch(currentState)
+		runTeleOp();
+	}
+	
+	//This is the function thats being called during Tele-Op
+	//The functions we call depend upon what kind of controller we are using to control the robot
+	//We can either control the robot with a normal x-box Joystick or with a steering wheel
+	public void runTeleOp()
+	{
+		if(steeringWheelDrive)
 		{
-			case 1:
-				mainpiston.dropMainPiston();
-				Timer.delay(1.5);
-				mainpiston.grabMainPiston();
-				Timer.delay(1.5);
-				currentState = 2;
-				break;
-			case 2:
-				if(enc.runOrNotForStraight(distanceforGearFromMiddle))
-				{
-					enc.encoderDrive(distanceforGearFromMiddle, 1);
-				}
-				else
-				{
-					Motors007.leftTalon.set(0);
-					Motors007.rightTalon.set(0);
-					currentState = 3;
-				}
-				break;
-			case 3:
-				autoWiggle();
-				mainpiston.dropMainPiston();
-				clampObj.unClampThePiston();
-				Timer.delay(.6);
-				enc.resetEncoders();
-				currentState = 4;
-				break;
-			case 4:
-				Motors007.leftTalon.set(0);
-				Motors007.rightTalon.set(0);
-				Timer.delay(.2);
-				mainpiston.grabMainPiston();
-				Timer.delay(.3);
-				enc.resetEncoders();
-				currentState = 5;
-				break;
-			case 5:
-				Motors007.leftTalon.set(0);
-				Motors007.rightTalon.set(0);
-				break;
+			joyStickValues.updateContollerValues();
+			updateAllPistonsForSteeringWheel();
+			climberObj.theClimber();
+			driveTrainForSteeringWheel();
+		}
+		else
+		{
+			joyStickValues.updateContollerValues();
+			updateAllPistons();
+			climberObj.theClimber();
+			driveTrain();
 		}
 	}
 	
+	//This is the piece of code that updates all the pistons for Tele-operated period
+	//This function assumes you are using a normal Xbox Controller
+	public void updateAllPistons()
+	{
+		clampObj.updatePistons();
+		mainpiston.updatePistons();
+		dropdown.updatePistons();
+	}
+		
+	//This is the piece of code that updates all the pistons for Tele-operated period
+	//This function assumes you are using the Big Joy-stick and the Steering Wheel
+	public void updateAllPistonsForSteeringWheel()
+	{
+		clampObj.updatePistons();
+		mainpiston.updatePistons();
+		dropdown.steeringWheelDropDown();
+	}
 	
+	//This is the code for the Drive-train
+	//This is the function that determines what type of drive-train we would like to use
+	public void driveTrain()
+	{
+		if(Joystick007.leftBumper)
+		{
+			teleOpWiggle();
+		}
+		else if(Joystick007.leftTrigger > 0)
+		{
+		
+			sloMoDrive();
+		}
+		else
+		{
+			updatedArcadeDrive();
+		}
+	}
+	
+	//This function tells us to use the arcade drive for the Steering Wheel Joy-stick
+	public void driveTrainForSteeringWheel()
+	{
+		updatedArcadeDriveForSteeringWheelDrive();
+	}
+	
+	
+	public void testPeriodic() 
+	{
+		// Does N O T H I N G
+	}
+	
+	//This function is when the driver would like to drive the robot in slow motion
+	//This function does not involve a P loop
+	public void sloMoDrive()
+	{
+		double leftValue, rightValue;
+		double checks,checkt,speed,turn;
+		leftValue = sinx(Joystick007.leftJoySticky);
+		rightValue = Joystick007.rightJoyStickx;
+		
+		checks = Math.abs(leftValue);
+		checkt = Math.abs(rightValue);
+		
+		if (checks <.15) 	
+		{
+			speed = 0;
+		}
+		else 			
+		{
+			speed = .3*(checks * leftValue);
+		}
+		if (checkt <.15) 	
+		{
+			turn = 0;
+		}
+		else 			
+		{
+			if(leftValue == 0)
+			{
+				turn = .3*(rightValue);
+			}
+			else
+			{
+				turn = .3*(rightValue);
+			}
+			
+		}    
+		Motors007.leftTalon.set(((speed+turn)));
+	    Motors007.rightTalon.set(((-speed+turn)));
+	}
+	
+	//This is the function we use for our normal drive-train for the X-box controller
+	//In this drive-train, we also use a P loop so that the robot can drive straight
+	//We use the P loop when prevState = 1 or 2
 	public void updatedArcadeDrive()
 	{
 		double leftValue, rightValue;
@@ -713,7 +759,9 @@ public class Robot extends IterativeRobot {
 		}	
 	}
 	
-	
+	//This is the function we use for our normal drive-train for the Steering Wheel controller
+	//In this drive-train, we also use a P loop so that the robot can drive straight
+	//We use the P loop when prevState = 1 or 2
 	public void updatedArcadeDriveForSteeringWheelDrive()
 	{
 		double leftValue, rightValue;
@@ -834,7 +882,7 @@ public class Robot extends IterativeRobot {
 		}	
 	}
 
-	
+	//This function takes the sin of the stickValue
 	public double sinx(double stickValue)
 	{
 		double calculatedValue = Math.PI * stickValue;
@@ -849,7 +897,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 	
-	
+	//This function makes the robot Wiggle during the Tele-Operated Period
 	public void teleOpWiggle()
 	{
 		switch(switchWiggle)
@@ -881,7 +929,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 	
-	
+	//This function makes the robot Wiggle during the Autonomous Period
 	public void autoWiggle()
 	{
 		for (int i = 1; i<=5;i++)
@@ -899,14 +947,15 @@ public class Robot extends IterativeRobot {
 		}
 	}
 	
-	
+	//This is the function we use to test our encoders
+	//We spin the encoders physically and get reading of the encoder values
 	public void testEncoders()
 	{
 		System.out.println("Right encoder value: " + enc.returnrightEncValue());
 		System.out.println("Left encoder value: " + enc.returnleftEncValue());
 	}
 	
-	
+	//This is the function we use to test what type of Autonomous we will be running
 	public void printAutoMode()
 	{
 		autoSelected = DigitalInputs.digitalPins();
